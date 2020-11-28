e907fcc0f8c2c4e568d435d8ac1dba6090b39038
zzx <zzx2867199593@qq.com>
2020-11-28 22:17:31 +0800

    zzx

---
+++ DBConnect.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace ShopSystem
+{
+    class DBConnect
+    {
+        private string conStr = "";
+
+        public bool AccountFind(string account,string password)
+        {
+            string sqlStr = string.Format("select count(*) from userInfo where uAccount = {0} AND uPassword = {1}", account, password);
+            SqlConnection conn = new SqlConnection(conStr);
+            SqlCommand cmd = new SqlCommand(sqlStr, conn); 
+            
+            conn.Open();
+            bool isFinded = (bool)cmd.ExecuteScalar();
+            conn.Close();
+            return isFinded;
+        }
+
+    }
+}
---
+++ loginForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace ShopSystem
+{
+    partial class loginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.account = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("宋体", 16F);
+            this.btnLogin.Location = new System.Drawing.Point(111, 265);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(310, 63);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "登  录";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.BtnLogin);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F);
+            this.label1.Location = new System.Drawing.Point(107, 165);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "用户名：";
+            // 
+            // account
+            // 
+            this.account.Location = new System.Drawing.Point(202, 165);
+            this.account.Name = "account";
+            this.account.Size = new System.Drawing.Size(219, 25);
+            this.account.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 12F);
+            this.label2.Location = new System.Drawing.Point(107, 210);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "密码：";
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(202, 210);
+            this.password.Name = "password";
+            this.password.PasswordChar = '*';
+            this.password.Size = new System.Drawing.Size(219, 25);
+            this.password.TabIndex = 4;
+            // 
+            // loginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 369);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.account);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLogin);
+            this.Name = "loginForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox account;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox password;
+    }
+}
